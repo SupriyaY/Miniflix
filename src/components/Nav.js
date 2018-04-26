8944229c223ccf78f3188d3093fa4486425df0ea
Supriya <supriya.yerramilli@gmail.com>
2018-04-26 09:54:01 -0400

    display component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,2 +1,43 @@
 //navigation that all page in the app will share
-import React, {Component} from 'react-router'
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { login, logout, isLoggedIn } from '../utils/AuthService';
+import '../App.css';
+
+
+class Nav extends Component {
+
+
+    render() {
+
+        return (
+            <nav className="navbar navbar-default">
+                <div className="navbar-header">
+                    <Link className="nav-brand" to="/">Miniflix</Link>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li>
+                        <Link to="/">All Videos</Link>
+                    </li>
+                    <li>
+                        {
+                            (isLoggedIn()) ? <Link to="/upload">Upload Vidoes</Link> : ''
+                        }
+                    </li>
+                </ul>
+                <ul className="nav navbar-nav navbar-right">
+                    <li>
+
+                        {
+                            (isLoggedIn()) ? (button)
+
+    }
+                    </li>
+                </ul>
+            </nav>
+        );
+
+    }
+}
+
+export default Nav;
